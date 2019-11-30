976b251c587159724e723e45f135aab8ae98bfa6
MrCano369 <42789396+MrCano369@users.noreply.github.com>
2019-11-29 22:55:07 -0600

    Add files via upload

--- estructura.js
+++ estructura.js
@@ -1,10 +1,41 @@
+class Pila{
+	constructor(){
+		this._ultimo = null;
+	}
+	get ultimo(){return this._ultimo;}
+
+	agregar(nuevo){
+		if (this._ultimo) {
+			nuevo.ant = this._ultimo;
+			this._ultimo = nuevo;
+		}else{
+			this._ultimo = nuevo;
+		}
+	}
+
+	sacar(){
+		var byeNodo = this._ultimo;
+		this._ultimo = byeNodo.ant;
+		return byeNodo;
+	}
+
+	imprimir(a = this._ultimo){
+		console.log(a.dato);
+		if (a.ant) {
+			this.imprimir(a.ant);
+		}
+	}
+}
+
 class ListaDoble{
 	constructor(){
 		this._primero = null;
 		this._ultimo = null;
+		this._pila = new Pila();
 	}
 	get primero(){return this._primero;}
 	get ultimo(){return this._ultimo;}
+	get pila(){return this._pila;}
 
 	set primero(v){this._primero = v;}
 	set ultimo(v){this._ultimo = v;}
@@ -33,7 +64,7 @@ class ListaDoble{
 		}
 	}
 
-	generarRaiz(nodo){
+	plantar(nodo){
 		nodo.izq = nodo.ant;
 		nodo.der = nodo.sig;
 		this.eliminar(nodo.ant);
@@ -44,7 +75,7 @@ class ListaDoble{
 		var actual = this._primero;
 		while(actual){
 			if (actual.dato == "*" || actual.dato == "/") {
-				this.generarRaiz(actual);
+				this.plantar(actual);
 			}
 			actual = actual.sig;
 		}
@@ -52,9 +83,35 @@ class ListaDoble{
 		actual = this._primero;
 		while(actual){
 			if (actual.dato == "+" || actual.dato == "-") {
-				this.generarRaiz(actual);
+				this.plantar(actual);
 			}
 			actual = actual.sig;
+		}
+
+		this.preOrder();
+
+
+	}
+
+	imprimir(p = this._primero){
+		console.log(p.dato);
+		if (p.sig) {
+			this.imprimir(p.sig);
+		}
+	}
+
+	calcular(){
+
+	}
+	
+	preOrder(r = this._primero){
+		console.log(r.dato);
+		this._pila.agregar(new Nodo(r.dato));
+		if (r.izq) {
+			this.preOrder(r.izq);
+		}
+		if (r.der) {
+			this.preOrder(r.der);
 		}
 	}
 }
